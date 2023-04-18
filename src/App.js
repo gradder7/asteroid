468a9d0a96542db34a08b6f3328139a2c145e06f
Himanshu <mehra3228@gmail.com>
2023-04-19 01:01:35 +0530

    Feagt:datePicker added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import SetDate from "./SetDate";
 import { fetchData } from "./utils/fetchData";
 function App() {
-  
 
-  const getData= async()=>{
-    let data=await fetchData("2022-01-07", "2022-01-08");
-    console.log(data);
-  }
-  getData();
   return (
-    <div className="App">
+    <div>
       <h1>Welocme to NASA</h1>
       <SetDate/>
     </div>
--- src/SetDate.jsx
+++ src/SetDate.jsx
@@ -1,20 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DateRangePicker } from "rsuite";
+import "rsuite/dist/rsuite.min.css";
+import { fetchData } from "./utils/fetchData";
 
 export default function SetDate() {
   const [date, setdate] = useState([]);
+  const [showData, setShowData] = useState(false);
   const handleOnchangeDate = (value) => {
     setdate(value);
   };
+  let dateStart =
+    date && date.length ? date[0].toISOString().slice(0, 10) : null;
+  let dateEnd = date && date.length ? date[1].toISOString().slice(0, 10) : null;
+
+  useEffect(() => {
+    const getData = async () => {
+      let data = await fetchData(dateStart, dateEnd);
+      console.log(data);
+    };
+    if (showData) {
+      getData();
+    }
+  }, [dateStart, dateEnd]);
+
+  const handleOk = () => {
+    setShowData(true);
+  };
+
   return (
-    <div>
+    <>
       <DateRangePicker
         onChange={handleOnchangeDate}
         value={date}
-        format="YYYY-MM-DD"
+        format="yyyy-MM-dd"
+        appearance="default"
+        onOk={handleOk}
       />
-      <p>Start date: {date[0]}</p>
-      <p>End date: {date[1]}</p>
-    </div>
+      <p>Start date: {dateStart}</p>
+      <p>End date: {dateEnd}</p>
+    </>
   );
 }
