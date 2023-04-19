7cab46e7bc0127653d7fe172c843d14669e1074b
Himanshu <mehra3228@gmail.com>
2023-04-19 16:17:49 +0530

    feat:label

--- src/components/SetDate.jsx
+++ src/components/SetDate.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { DateRangePicker } from "rsuite";
+import { DateRangePicker, SelectPicker } from "rsuite";
 import "rsuite/dist/rsuite.min.css";
 import { fetchData } from "../utils/fetchData";
 import Chart from "chart.js/auto";
@@ -9,8 +9,7 @@ export default function SetDate() {
   const [showData, setShowData] = useState(false);
   const [asteroidData, setAsteroidData] = useState({});
   const [chartData, setChartData] = useState({});
-  // state
-  const [selectedLabel, setSelectedLabel] = useState(null);
+  const [selectedLabel, setSelectedLabel] = useState("fastestAsteroid");
 
   const handleOnchangeDate = (value) => {
     setdate(value);
@@ -36,21 +35,23 @@ export default function SetDate() {
       let closestAsteroid = [];
       let avgSizeAsteroid = [];
       let numAsteroids = [];
-      for (const [date, asteroids] of Object.entries(asteroidData)) {
-        let fastestAsteroidForDate = asteroids.reduce(
-          (prev, curr) =>
-            parseFloat(
-              curr.close_approach_data[0].relative_velocity
-                .kilometers_per_second
-            ) >
-            parseFloat(
-              prev.close_approach_data[0].relative_velocity
-                .kilometers_per_second
-            )
-              ? curr
-              : prev,
-          asteroids[0]
+      for (const date of labels) {
+        const asteroids = asteroidData[date];
+        let fastestAsteroidForDate = asteroids[0];
+        let fastestAsteroidVelocity = parseFloat(
+          asteroids[0].close_approach_data[0].relative_velocity
+            .kilometers_per_second
         );
+        for (const asteroid of asteroids) {
+          const asteroidVelocity = parseFloat(
+            asteroid.close_approach_data[0].relative_velocity
+              .kilometers_per_second
+          );
+          if (asteroidVelocity > fastestAsteroidVelocity) {
+            fastestAsteroidVelocity = asteroidVelocity;
+            fastestAsteroidForDate = asteroid;
+          }
+        }
         fastestAsteroid.push(
           parseFloat(
             fastestAsteroidForDate.close_approach_data[0].relative_velocity
@@ -58,106 +59,132 @@ export default function SetDate() {
           ).toFixed(2)
         );
 
-        let closestAsteroidForDate = asteroids.reduce(
-          (prev, curr) =>
-            parseFloat(curr.close_approach_data[0].miss_distance.kilometers) <
-            parseFloat(prev.close_approach_data[0].miss_distance.kilometers)
-              ? curr
-              : prev,
-          asteroids[0]
+        let closestAsteroidForDate = asteroids[0];
+        let closestAsteroidDistance = parseFloat(
+          asteroids[0].close_approach_data[0].miss_distance.kilometers
         );
-        closestAsteroid.push(
-          parseFloat(
-            closestAsteroidForDate.close_approach_data[0].miss_distance
-              .kilometers
-          ).toFixed(2)
+        for (const asteroid of asteroids) {
+          const asteroidDistance = parseFloat(
+            asteroid.close_approach_data[0].miss_distance.kilometers
+          );
+          if (asteroidDistance < closestAsteroidDistance) {
+            closestAsteroidDistance = asteroidDistance;
+            closestAsteroidForDate = asteroid;
+          }
+        }
+        closestAsteroid.push(closestAsteroidDistance.toFixed(2));
+
+        let avgSizeAsteroidForDate = 0;
+        for (const asteroid of asteroids) {
+          avgSizeAsteroidForDate +=
+            (parseFloat(
+              asteroid.estimated_diameter.kilometers.estimated_dd_diameter_min
+            ) +
+              parseFloat(
+                asteroid.estimated_diameter.kilometers.estimated_diameter_max
+              )) /
+            2;
+        }
+        avgSizeAsteroid.push(
+          (avgSizeAsteroidForDate / asteroids.length).toFixed(2)
         );
-
-        let avgSizeAsteroidForDate =
-          asteroids.reduce(
-            (prev, curr) =>
-              prev +
-              (parseFloat(
-                curr.estimated_diameter.kilometers.estimated_diameter_min
-              ) +
-                parseFloat(
-                  curr.estimated_diameter.kilometers.estimated_diameter_max
-                )) /
-                2,
-            0
-          ) / asteroids.length;
-        avgSizeAsteroid.push(avgSizeAsteroidForDate.toFixed(2));
-
         numAsteroids.push(asteroids.length);
       }
 
-      setChartData({
+      const data = {
         labels: labels,
         datasets: [
           {
             label: "Fastest Asteroid (km/h)",
             data: fastestAsteroid,
-            backgroundColor: "rgba(255, 99,0, 0.2)",
-            borderColor: "rgba(255, 99, 0, 1)",
+            borderColor: "rgba(255, 99, 132, 1)",
             borderWidth: 1,
           },
           {
             label: "Closest Asteroid (km)",
             data: closestAsteroid,
-            backgroundColor: "rgba(54, 162, 235, 0.2)",
             borderColor: "rgba(54, 162, 235, 1)",
             borderWidth: 1,
           },
           {
             label: "Average Size of Asteroids (km)",
             data: avgSizeAsteroid,
-            backgroundColor: "rgba(255, 206, 86, 0.2)",
             borderColor: "rgba(255, 206, 86, 1)",
             borderWidth: 1,
           },
           {
             label: "Number of Asteroids",
             data: numAsteroids,
-            backgroundColor: "rgba(75, 192, 192, 0.2)",
             borderColor: "rgba(75, 192, 192, 1)",
             borderWidth: 1,
           },
         ],
-      });
+      };
+      setChartData(data);
     }
   }, [asteroidData]);
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    setShowData(true);
-    setSelectedLabel(chartData.datasets[0].label);
+  const handleOnchangeLabel = (value) => {
+    setSelectedLabel(value);
+    destroyCanvas();
+  };
+  const destroyCanvas = () => {
+    const canvas = document.getElementById("myChart");
+    if (canvas) {
+      canvas.parentNode.removeChild(canvas);
+    }
   };
 
   useEffect(() => {
     if (Object.keys(chartData).length !== 0) {
-      const ctx = document.getElementById("chart");
+      const ctx = document.getElementById("myChart");
       new Chart(ctx, {
         type: "bar",
         data: chartData,
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
       });
     }
-  }, [chartData]);
+  }, [chartData, selectedLabel]);
 
   return (
-    <div className="container">
-      <form onSubmit={handleOnSubmit}>
-        <DateRangePicker
-          style={{ width: 400 }}
-          value={date}
-          onChange={handleOnchangeDate}
-        />
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
-      <div className="chart-container">
-        <canvas id="chart"></canvas>
-      </div>
+    <div>
+      <h2>Choose a date range to show asteroid data</h2>
+      <DateRangePicker onChange={handleOnchangeDate} />
+      <br />
+      <br />
+      <button onClick={() => setShowData(true)}>Show Data</button>
+      <br />
+      <br />
+      {Object.keys(chartData).length !== 0 && (
+        <>
+          <SelectPicker
+            data={[
+              { label: "Fastest Asteroid (km/h)", value: "fastestAsteroid" },
+              { label: "Closest Asteroid (km)", value: "closestAsteroid" },
+              {
+                label: "Average Size of Asteroids (km)",
+                value: "avgSizeAsteroid",
+              },
+              { label: "Number of Asteroids", value: "numAsteroids" },
+            ]}
+            searchable={false}
+            cleanable={false}
+            onChange={handleOnchangeLabel}
+            defaultValue="fastestAsteroid"
+          />
+          <canvas id="myChart" width="400" height="400"></canvas>
+        </>
+      )}
     </div>
   );
 }
